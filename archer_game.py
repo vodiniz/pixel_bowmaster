4d4b201a5e1c07c78a995f59cf5d69f8a742046c
Vodiniz <vitor.oliveira.vod@gmail.com>
2021-08-14 21:27:07 -0300

    next:arrow

--- archer_game.py
+++ archer_game.py
@@ -1,5 +1,3 @@
-from email.mime import image
-from lib2to3.pytree import convert
 import pygame
 from pygame.locals import *
 
@@ -10,38 +8,63 @@ class Archer(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.images = [(pygame.image.load('Assets/character/character_sprite_pos1.png')).convert_alpha(),
-                    pygame.image.load('Assets/character/character_sprite_pos2.png').convert_alpha(),
-                    pygame.image.load('Assets/character/character_sprite_pos3.png').convert_alpha(),
-                    pygame.image.load('Assets/character/character_sprite_pos4.png').convert_alpha()
+        self.images = [(pygame.image.load('Assets/character/character_sprite_idle1.png')).convert_alpha(),
+                    pygame.image.load('Assets/character/character_sprite_idle2.png').convert_alpha()
         ]
         
         for i in range(len(self.images)):
             self.images[i] = pygame.transform.scale(self.images[i], (150, 150))
 
+        self.shooting_images = [(pygame.image.load('Assets/character/character_sprite_shooting1.png')).convert_alpha(),
+                    pygame.image.load('Assets/character/character_sprite_shooting2.png').convert_alpha(),
+                    pygame.image.load('Assets/character/character_sprite_shooting3.png').convert_alpha(),
+                    pygame.image.load('Assets/character/character_sprite_shooting4.png').convert_alpha()
+        ]
+        
+        for i in range(len(self.shooting_images)):
+            self.shooting_images[i] = pygame.transform.scale(self.shooting_images[i], (150, 150))
+
         self.current_image = 0
 
         self.archer_position = [20,20]
 
-        self.image = pygame.image.load('Assets/character/character_sprite_pos1.png').convert_alpha()
+        self.image = pygame.image.load('Assets/character/character_sprite_idle1.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (150, 150))
         self.rect = self.image.get_rect()
 
         self.moving_up = False
         self.moving_down = False
+        self.shooting = Falseq
+        self.shooting_ready = False
         
     def update(self):
-        self.current_image += 0.15
-        if self.current_image >= len(self.images):
-            self.current_image = 0
+        if self.current_image >= 3:
+            self.shooting_ready = True
+        else:
+            self.shooting_ready = False
 
-        self.image = self.images[int(self.current_image)]
+        self.current_image += 0.15
+
+        if self.shooting:
+            if self.current_image >= len(self.shooting_images):
+                self.current_image = 3
+        else:
+            self.current_image -= 0.07
+            if self.current_image >= len(self.images):
+                self.current_image = 0
+
+
+        if self.shooting:
+            self.image = self.shooting_images[int(self.current_image)]
+        else:
+            self.image = self.images[int(self.current_image)]
+
 
         if self.moving_up == True:
             self.archer_position[1] -= 8
-    
         if self.moving_down == True:
             self.archer_position[1] += 8
+
 
         self.rect[0] = self.archer_position[0]
         self.rect[1] = self.archer_position[1]
@@ -77,24 +100,39 @@ class Balloon(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
         self.popped = False
+        
+
+    def is_off_screen(self):
+        if self.rect[1] <= -200:
+            return True
+        else:
+            return False
+                
 
     def update(self):
         if self.popped == True:
             pass
-        
-
         self.current_image += 0.15
         if self.current_image >= len(self.balloon_images):
             self.current_image = 0
         self.image = self.balloon_images[int(self.current_image)]
-
-
         self.balloon_position[1] -= 1.3
+
+        if self.is_off_screen() and self.popped == False:
+            self.balloon_position[1] = 900
+
         self.rect[1] = self.balloon_position[1]
+    
 
 
 
-        
+class Arrow(pygame.sprite.Sprite):
+
+    def __init__(self):
+        self.speed = 10
+
+
+
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -103,6 +141,7 @@ BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDTH, SCREEN_HEIGHT))
 FLOOR = (pygame.image.load('Assets/textures/wooden_floor.png')).convert()
 FLOOR_CROP = FLOOR.subsurface(0, 0, 40, 200)
 FLOOR = pygame.transform.scale(FLOOR_CROP, (150,800))
+FIRST_BALLOON_DISTANCE = 600
 
 
 archer_group = pygame.sprite.Group()
@@ -110,8 +149,10 @@ archer = Archer()
 archer_group.add(archer)
 
 balloon_group = pygame.sprite.Group()
-balloon1 = Balloon(400)
-balloon_group.add(balloon1)
+
+for i in range(15):
+    new_balloon = Balloon(FIRST_BALLOON_DISTANCE+i*50)
+    balloon_group.add(new_balloon)
 
 
 clock = pygame.time.Clock()
@@ -129,6 +170,10 @@ while True:
                 archer.moving_up = True
             if event.key == K_DOWN:
                 archer.moving_down = True
+            if event.key == K_SPACE:
+                archer.shooting = True
+                if archer.shooting_ready == True:
+                    pass
 
         if event.type == KEYUP:
             if event.key == K_UP:
